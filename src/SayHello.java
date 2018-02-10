a0f3258c175707f54c359e8dbccba7a4b6ad5f74
Kent Collins <kent.r.collins@gmail.com>
2018-02-10 18:11:52 +0000

    implemented testing

--- src/SayHello.java
+++ src/SayHello.java
@@ -2,13 +2,13 @@
 public class SayHello {
 
 	/**
-	 * Write a statement that prints the phrase "Hello, World!"
+	 * Write a statement that prints the phrase "Hello, world!"
 	 * to the system output.
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		//TODO
 	}
 
 }
