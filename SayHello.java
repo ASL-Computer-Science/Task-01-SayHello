19aac6abb560b73b6cd6e6fee31babeb73f621e7
Kent Collins <kent.r.collins@gmail.com>
2018-02-10 17:26:25 +0000

    added main class

---
+++ SayHello.java
@@ -0,0 +1,14 @@
+
+public class SayHello {
+
+	/**
+	 * Write a statement that prints the phrase "Hello, World!"
+	 * to the system output.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
