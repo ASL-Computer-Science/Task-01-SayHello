64b857b8ca2ce6d4cf476b2d21ccbb5feed1c6c1
Kent Collins <kent.r.collins@gmail.com>
2018-02-12 06:42:25 +0000

    placed correct tests over templates

---
+++ sayhello/src/main/java/org/asl/apcs/sayhello/SayHello.java
@@ -0,0 +1,15 @@
+package org.asl.apcs.sayhello;
+
+public class SayHello {
+
+	/**
+	 * Write a statement that prints the phrase "Hello, world!"
+	 * to the system output.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//TODO
+	}
+
+}
